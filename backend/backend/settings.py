aaf7e8115c57c52199fdc91b411e6786f45c93c2
Bittu169 <mondalbittu169@gmail.com>
2026-06-22 11:58:36 +0530

    Update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,10 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config(
-    'SECRET_KEY',
-    default='django-insecure-%a05&+eyh5y9hqjs3!d+1z!o$n!+-r#03p@i@8ar6klgwv^t6a'
-)
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
